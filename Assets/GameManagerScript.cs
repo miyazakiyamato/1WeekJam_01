5aaf40f6be018f6199581911e7716e64934eb094
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-05-27 11:20:03 +0900

    test

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,15 +4,145 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    public GameObject playerPreFab;
+    public GameObject boxPreFab;
+    public GameObject noBreakBoxPreFab;
+    public GameObject enemyPreFab;
+    public GameObject goalPreFab;
+    public GameObject clearText;
+
+    int[,] map;
+    GameObject[,] field;
+    List<GameObject> goalsField;
+    bool IsCleared()
+    {
+        List<Vector2Int> goals = new List<Vector2Int>();
+        //
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 3)
+                {
+                    goals.Add(new Vector2Int(x, y));
+                }
+            }
+        }
+        //
+        //for (int i = 0; i < goals.Count; i++)
+        //{
+        //    GameObject f = field[goals[i].y, goals[i].x];
+        //    if (f == null || f.tag != "Player")
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        return true;
+    }
+    void Reset()
+    {
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                Destroy(field[y, x]);
+            }
+        }
+        for (int i = 0; i < goalsField.Count; i++)
+        {
+            Destroy(goalsField[i]);
+        }
+        Start();
+        clearText.SetActive(IsCleared());
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.SetResolution(1920, 1080, true);
+
+        map = new int[,] {
+            { 4,4,4,4,4,4,4,4,4,4,4},
+            { 4,0,0,0,0,0,0,0,0,3,4},
+            { 4,1,0,0,0,2,0,0,5,0,4},
+            { 4,0,0,0,2,2,2,0,0,0,4},
+            { 4,0,0,0,0,2,0,0,0,0,4},
+            { 4,0,0,0,0,0,0,0,0,0,4},
+            { 4,0,0,0,0,0,0,0,0,0,4},
+            { 4,0,0,0,0,0,0,0,0,0,4},
+            { 4,4,4,4,4,4,4,4,4,4,4},
+            };
+
+        field = new GameObject[map.GetLength(0), map.GetLength(1)];
+        goalsField = new List<GameObject>();
+
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 1)
+                {
+                    field[y, x] = Instantiate(
+                        playerPreFab,
+                        new Vector3(x - map.GetLength(1) / 2, -y + map.GetLength(0) / 2, 0),
+                        Quaternion.identity
+                        );
+                }
+                if (map[y, x] == 2)
+                {
+                    field[y, x] = Instantiate(
+                        boxPreFab,
+                        new Vector3(x - map.GetLength(1) / 2, -y + map.GetLength(0) / 2, 0),
+                        Quaternion.identity
+                        );
+                }
+                if (map[y, x] == 3)
+                {
+                    goalsField.Add(Instantiate(
+                        goalPreFab,
+                        new Vector3(x - map.GetLength(1) / 2, -y + map.GetLength(0) / 2, 0.01f),
+                        Quaternion.identity
+                        ));
+                }
+                if (map[y, x] == 4)
+                {
+                    field[y, x] = Instantiate(
+                        noBreakBoxPreFab,
+                        new Vector3(x - map.GetLength(1) / 2, -y + map.GetLength(0) / 2, 0),
+                        Quaternion.identity
+                        );
+                }
+                if (map[y, x] == 5)
+                {
+                    field[y, x] = Instantiate(
+                        enemyPreFab,
+                        new Vector3(x - map.GetLength(1) / 2, -y + map.GetLength(0) / 2, 0),
+                        Quaternion.identity
+                        );
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!IsCleared())
+        {
+           
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+              
+                Reset();
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
     }
 }
